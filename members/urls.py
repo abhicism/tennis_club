d1103ec433c51934b9e8d0d837f7ee473de53239
abhicism <abhicism@gmail.com>
2026-02-15 10:00:12 +0530

    comments has been added in urls file in app and root folder

--- members/urls.py
+++ members/urls.py
@@ -1,7 +1,7 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
+from django.urls import path #import (path)the url routing function used to map a URL to a view
+from . import views  #import views.py from the same folder (current app)
+ 
+urlpatterns = [     #dj looks for this list to know all routes of this app
     path('', views.main, name='main'),
     path('members/', views.members, name='members'),
     path('members/details/<int:id>', views.details, name='details'),
--- my_tennis_club/urls.py
+++ my_tennis_club/urls.py
@@ -1,7 +1,8 @@
-from django.contrib import admin
-from django.urls import include, path
+from django.contrib import admin  #import dj admin site
+from django.urls import include, path  #path->define route, include ->forward to app urls
 
 urlpatterns = [
-    path('', include('members.urls')),
-    path('admin/', admin.site.urls),
+    path('', include('members.urls')),  #when user visits root url "/" send request to member app urls.py
+    # ' ' is empty string represent the root url (homepage) of website
+    path('admin/', admin.site.urls),   #when user visits "/admin/" , open django admin panel (open dj admin interface)
 ]
