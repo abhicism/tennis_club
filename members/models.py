33127f1d006bdd24ba4f2e4d2efa4f6925365f76
abhicism <abhicism@gmail.com>
2026-02-18 11:20:07 +0530

    comments added in the models file.

--- members/models.py
+++ members/models.py
@@ -1,6 +1,6 @@
-from django.db import models
+from django.db import models  # Import Django's ORM base classes used to define database tables
 
-class Member(models.Model):
+class Member(models.Model):   # Create a database model (table) named Member
   firstname = models.CharField(max_length=255)
   lastname = models.CharField(max_length=255)
   phone = models.IntegerField(null=True)
