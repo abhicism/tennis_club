91dd07dcfddaddff15f09061fbcd3c8679a85a45
abhicism <abhicism@gmail.com>
2026-03-24 20:24:16 +0530

    changes done in serializers file

--- members/serializers.py
+++ members/serializers.py
@@ -1,7 +1,10 @@
 from rest_framework import serializers
 from .models import Member
 
+
 class MemberSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')  # ✅ FIX
+
     class Meta:
         model = Member
         fields = '__all__'
