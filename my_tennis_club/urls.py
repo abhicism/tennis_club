a43656bde3c4226d8d7b5708592c516152c5974e
abhicism <abhicism@gmail.com>
2026-03-24 09:47:48 +0530

    jwt files added

--- my_tennis_club/urls.py
+++ my_tennis_club/urls.py
@@ -1,8 +1,13 @@
-from django.contrib import admin  #import dj admin site
-from django.urls import include, path  #path->define route, include ->forward to app urls
+from django.contrib import admin  # import dj admin site
+from django.urls import include, path  # path -> define route, include -> forward to app urls
+
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
-    path('', include('members.urls')),  #when user visits root url "/" send request to member app urls.py
-    # ' ' is empty string represent the root url (homepage) of website
-    path('admin/', admin.site.urls),   #when user visits "/admin/" , open django admin panel (open dj admin interface)
+    path('', include('members.urls')),  # root → members app
+    path('admin/', admin.site.urls),   # admin panel
+
+    # ✅ JWT Authentication endpoints
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
